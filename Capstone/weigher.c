c0d8d082afc6cf19b7bc3064bf288fd131d24fe1
Scott <s.kirk.r@gmail.com>
2017-12-03 00:50:24 -0500

    Trial and Error

--- Capstone/weigher.c
+++ Capstone/weigher.c
@@ -7,21 +7,27 @@
 #define READ_SDO (P1IN&BIT6)
 #define SELECT_HIGH P2OUT |= BIT0;
 #define SELECT_LOW P2OUT &= ~BIT0;
+#define TESTBIT_HIGH P2OUT |= BIT4;
+#define TESTBIT_LOW P2OUT &= ~BIT4;
 
 long get_data(void) {
     SELECT_LOW;
-    long sampleData = 0x00;
+    long sampleData = 0x0000;
     while(READ_CLOCK != 0x00);
     while(READ_CLOCK == 0x00);
     if(READ_SDO != 0) { //First bit is push on rising edge
         sampleData |= 0x01;
+        TESTBIT_HIGH;
     }
     int i = 0;
     for(;i < 31; i++)   {
         sampleData = sampleData << 1;
         while(READ_CLOCK != 0x00);
-        if(READ_SDO == 0)   {
+        if(READ_SDO != 0)   {
             sampleData |= 0x01;
+            TESTBIT_HIGH;
+        }   else    {
+            TESTBIT_LOW;
         }
         while(READ_CLOCK == 0x00);
     }
@@ -33,6 +39,7 @@ void init_sensor(void)  {
     P1DIR &= ~BIT5; //SCK
     P2DIR |= BIT0; //CS
     P1DIR &= ~BIT6; //SDO
+    P2DIR |= BIT4;
     SELECT_HIGH; //CS High
 }
 
